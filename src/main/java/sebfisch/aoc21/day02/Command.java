9c7c4684d7b4909730a288b7c931772aa753ea87
Sebastian Fischer <mail@sebfisch.de>
2022-01-06 00:26:04 +0100

    text block

--- src/main/java/sebfisch/aoc21/day02/Command.java
+++ src/main/java/sebfisch/aoc21/day02/Command.java
@@ -34,13 +34,14 @@ public sealed interface Command {
     }
 
     static Stream<Command> exampleInput() {
-        return """
-                forward 5
-                down 5
-                forward 8
-                up 3
-                down 8
-                forward 2
-                """.lines().map(Command::parse);
+        return
+            """
+            forward 5
+            down 5
+            forward 8
+            up 3
+            down 8
+            forward 2
+            """.lines().map(Command::parse);
     }
 }
