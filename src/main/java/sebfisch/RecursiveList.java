777e307f6cbdbd9cbfbba2b456210f626e3505af
Sebastian Fischer <mail@sebfisch.de>
2022-01-05 04:00:19 +0100

    solve instanceof tasks

--- src/main/java/sebfisch/RecursiveList.java
+++ src/main/java/sebfisch/RecursiveList.java
@@ -1,6 +1,8 @@
 package sebfisch;
 
 import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public sealed interface RecursiveList<T> {
     record Empty<T> () implements RecursiveList<T> {
@@ -18,5 +20,33 @@ public sealed interface RecursiveList<T> {
         return new Populated<>(
                 args[0],
                 of(Arrays.copyOfRange(args, 1, args.length)));
+    }
+
+    default int length() {
+        if (this instanceof Populated<T> self) {
+            return 1 + self.tail.length();
+        }
+
+        return 0;
+    }
+
+    default <U> RecursiveList<U> map(Function<T,U> fun) {
+        if (this instanceof Populated<T> self) {
+            return new Populated<>(fun.apply(self.head), self.tail.map(fun));
+        }
+
+        return new Empty<>();
+    }
+
+    default RecursiveList<T> filter(Predicate<T> pred) {
+        if (this instanceof Populated<T> self) {
+            final RecursiveList<T> filteredTail = self.tail.filter(pred);
+            if (pred.test(self.head)) {
+                return new Populated<>(self.head, filteredTail);
+            }
+            return filteredTail;
+        }
+
+        return this;
     }
 }
--- src/test/java/sebfisch/RecursiveListTest.java
+++ src/test/java/sebfisch/RecursiveListTest.java
@@ -18,4 +18,22 @@ public class RecursiveListTest {
                 (RecursiveList.Populated<Integer>) RecursiveList.of(42, 43);
         assertEquals(RecursiveList.of(43), list.tail());
     }
+
+    @Test
+    void testLength() {
+        assertEquals(3, RecursiveList.of(1,2,3).length());
+    }
+
+    @Test
+    void testMap() {
+        RecursiveList<String> strings = RecursiveList.of("hi","there");
+        RecursiveList<Integer> actual = strings.map(String::length);
+        assertEquals(RecursiveList.of(2,5), actual);
+    }
+
+    @Test void testFilter() {
+        RecursiveList<String> strings = RecursiveList.of("hi","there");
+        RecursiveList<String> actual = strings.filter(s -> s.length() < 3);
+        assertEquals(RecursiveList.of("hi"), actual);
+    }
 }
