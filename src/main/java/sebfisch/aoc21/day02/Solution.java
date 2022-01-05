179d9dd535b0884b0dcebf887091325208672978
Sebastian Fischer <mail@sebfisch.de>
2022-01-05 22:48:57 +0100

    rename AoC program

--- src/main/java/sebfisch/aoc21/day02/Solution.java
+++ src/main/java/sebfisch/aoc21/day02/Solution.java
@@ -1,6 +1,6 @@
 package sebfisch.aoc21.day02;
 
-public class Task01 {
+public class Solution {
     // TODO: Task 3.1 - implement task from Advent of Code 2021, Day 2
 
     // TODO: Task 6.1 - refactor your solution using features from the second part of the tutorial
