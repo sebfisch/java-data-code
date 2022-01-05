0b74a609bc5b3a2a80b942aa2baa51b5fce9fee8
Sebastian Fischer <mail@sebfisch.de>
2022-01-05 12:36:36 +0100

    add switch example for base colors

--- src/main/java/sebfisch/colors/BaseColor.java
+++ src/main/java/sebfisch/colors/BaseColor.java
@@ -1,14 +1,84 @@
 package sebfisch.colors;
 
-public enum BaseColor { 
+public enum BaseColor {
     RED, GREEN, BLUE;
 
     public static String asHexString(BaseColor color) {
+        return switch (color) {
+            case RED -> "#ff0000";
+            case GREEN -> "#00ff00";
+            case BLUE -> "#0000ff";
+        };
+    }
+
+    public static String describe1(BaseColor color) {
         switch (color) {
-            case RED: return "#ff0000";
-            case GREEN: return "#00ff00";
-            case BLUE: return "#0000ff";
-            default: throw new IllegalArgumentException("unreachable");
+            case RED:
+                return "hot";
+            case GREEN, BLUE:
+                return "cold";
+            default:
+                throw new IllegalArgumentException("unreachable");
         }
+    }
+
+    public static String describe2(BaseColor color) {
+        switch (color) {
+            case RED:
+                return "hot";
+            case GREEN:
+            case BLUE:
+                return "cold";
+            default:
+                throw new IllegalArgumentException("unreachable");
+        }
+    }
+
+    public static String describe3(BaseColor color) {
+        switch (color) {
+            case RED -> {
+                return "hot";
+            }
+            case GREEN, BLUE -> {
+                return "cold";
+            }
+            default -> throw new IllegalArgumentException("unreachable");
+        }
+    }
+
+    public static String describe4(BaseColor color) {
+        return switch (color) {
+            case RED:
+                yield "hot";
+            case GREEN, BLUE:
+                yield "cold";
+        };
+    }
+
+    public static String describe5(BaseColor color) {
+        return switch (color) {
+            case RED:
+                yield "hot";
+            case GREEN:
+            case BLUE:
+                yield "cold";
+        };
+    }
+
+    public static String describe6(BaseColor color) {
+        return switch (color) {
+            case RED -> "hot";
+            case GREEN, BLUE -> {
+                final String cold = "c" + "o" + "l" + "d";
+                yield cold;
+            }
+        };
+    }
+
+    public static String describe7(BaseColor color) {
+        return switch (color) {
+            case RED -> "hot";
+            case GREEN, BLUE -> "cold";
+        };
     }
 }
