5bcca0f4ea98ef81e1b12fd45c73e1fc8e5c0e3c
Sebastian Fischer <mail@sebfisch.de>
2022-01-05 10:05:08 +0100

    reduce test visibility

--- src/test/java/sebfisch/OptionalTest.java
+++ src/test/java/sebfisch/OptionalTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class OptionalTest {
+class OptionalTest {
     @Test
     void testPresent() {
         assertEquals("x", new Optional.Present<>("x").value());
