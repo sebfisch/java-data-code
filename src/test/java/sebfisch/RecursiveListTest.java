9e140da50c64f89b3618d247310849a4af15d2f1
Sebastian Fischer <mail@sebfisch.de>
2022-01-05 10:05:52 +0100

    reduce test visibility

--- src/test/java/sebfisch/RecursiveListTest.java
+++ src/test/java/sebfisch/RecursiveListTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class RecursiveListTest {
+class RecursiveListTest {
     @Test
     void testHead() {
         RecursiveList.Populated<Integer> list = //
