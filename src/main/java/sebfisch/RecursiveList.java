da9f6d278c1e3d0e9c3d96b09228d833f267b9f9
Sebastian Fischer <mail@sebfisch.de>
2022-01-05 22:09:20 +0100

    add todos

--- src/main/java/sebfisch/RecursiveList.java
+++ src/main/java/sebfisch/RecursiveList.java
@@ -4,4 +4,6 @@ public interface RecursiveList<T> {
     // TODO: Task 2.1 - implement recursive lists as sealed interface with nested records
 
     // TODO: Task 4.1 - implement length, map, and filter functions as default methods
+
+    // TODO: Task 5.1 - refactor length, map, and filter to use patterns in switch expressions
 }
--- src/main/java/sebfisch/aoc21/day02/Task01.java
+++ src/main/java/sebfisch/aoc21/day02/Task01.java
@@ -2,4 +2,6 @@ package sebfisch.aoc21.day02;
 
 public class Task01 {
     // TODO: Task 3.1 - implement task from Advent of Code 2021, Day 2
+
+    // TODO: Task 6.1 - refactor your solution using features from the second part of the tutorial
 }
