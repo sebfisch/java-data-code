10ad6c1a54e1d9d8abe742002c548c485e014c1a
Sebastian Fischer <mail@sebfisch.de>
2022-01-06 11:08:57 +0100

    rename color test

--- src/test/java/sebfisch/colors/RGBColorTest.java
+++ src/test/java/sebfisch/colors/RGBColorTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
-class ColorTest {
+class RGBColorTest {
     @Test
     void testRGBdescriptionWithDefaultBranchForNull() {
         assertThrows(NullPointerException.class, () -> {
